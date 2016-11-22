383fb651e96e6149c49a93b5223b75f9ecf2a710
chrishor29 <chrishor29@gmail.com>
2016-11-22 15:15:10 +0100

    make some changes

---
+++ update.bat
@@ -0,0 +1,8 @@
+copy F:\LEARN\KEMIA\kemia.html I:\Users\chrishor29\Desktop\learn\ok\kemia.html
+copy F:\LEARN\javascript.js I:\Users\chrishor29\Desktop\learn\javascript.js
+copy F:\LEARN\style.css I:\Users\chrishor29\Desktop\learn\style.css
+copy F:\LEARN\KEMIA\images\*.* I:\Users\chrishor29\Desktop\learn\ok\images
+git add .
+git commit -m "make some changes"
+git push
+Pause
