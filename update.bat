9b0435c7389f30eca69bd9071d53bdfa450c82d6
chrishor29 <chrishor29@gmail.com>
2016-12-04 18:05:53 +0100

    make some changes

--- update.bat
+++ update.bat
@@ -2,9 +2,10 @@ copy F:\LEARN\javascript.js I:\Users\chrishor29\Desktop\learn\javascript.js
 copy F:\LEARN\style.css I:\Users\chrishor29\Desktop\learn\style.css
 
 copy F:\LEARN\KEMIA\kemia.html I:\Users\chrishor29\Desktop\learn\ok\kemia.html
-copy F:\LEARN\KEMIA\images\*.* I:\Users\chrishor29\Desktop\learn\ok\images
+copy F:\LEARN\KEMIA\favicon.bmp I:\Users\chrishor29\Desktop\learn\ok\favicon.bmp
+robocopy F:\LEARN\KEMIA\images I:\Users\chrishor29\Desktop\learn\ok\images /mir
 
-copy F:\LEARN\FejlBiosz\images\*.* I:\Users\chrishor29\Desktop\learn\fb\images
+robocopy F:\LEARN\FejlBiosz\images\*.* I:\Users\chrishor29\Desktop\learn\fb\images /mir
 copy F:\LEARN\FejlBiosz\FejlBiosz.html I:\Users\chrishor29\Desktop\learn\fb\FejlBiosz.html
 
 git add .
