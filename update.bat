d9058d4d1d964e1e2cae37a296a932ce2bdebad3
chrishor29 <chrishor29@gmail.com>
2016-12-10 15:19:02 +0100

    make some changes

--- update.bat
+++ update.bat
@@ -5,9 +5,6 @@ copy F:\LEARN\KEMIA\kemia.html I:\Users\chrishor29\Desktop\learn\ok\kemia.html
 copy F:\LEARN\KEMIA\favicon.bmp I:\Users\chrishor29\Desktop\learn\ok\favicon.bmp
 robocopy F:\LEARN\KEMIA\images I:\Users\chrishor29\Desktop\learn\ok\images /mir
 
-robocopy F:\LEARN\FejlBiosz\images I:\Users\chrishor29\Desktop\learn\fb\images /mir
-copy F:\LEARN\FejlBiosz\FejlBiosz.html I:\Users\chrishor29\Desktop\learn\fb\FejlBiosz.html
-
 git add .
 git commit -m "make some changes"
 git push
