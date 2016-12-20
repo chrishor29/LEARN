d4ab8409d264555fc77f09fdd2f1b5c07fb0cf92
chrishor29 <chrishor29@gmail.com>
2016-12-20 10:51:51 +0100

    make some changes

--- update.bat
+++ update.bat
@@ -4,6 +4,8 @@ copy F:\LEARN\style.css I:\Users\chrishor29\Desktop\learn\style.css
 copy F:\LEARN\KEMIA\kemia.html I:\Users\chrishor29\Desktop\learn\ok\kemia.html
 copy F:\LEARN\KEMIA\favicon.bmp I:\Users\chrishor29\Desktop\learn\ok\favicon.bmp
 robocopy F:\LEARN\KEMIA\images I:\Users\chrishor29\Desktop\learn\ok\images /mir
+copy F:\LEARN\KEMIA\style.css I:\Users\chrishor29\Desktop\learn\ok\style.css
+copy F:\LEARN\KEMIA\javascript.js I:\Users\chrishor29\Desktop\learn\ok\javascript.js
 
 git add .
 git commit -m "make some changes"
