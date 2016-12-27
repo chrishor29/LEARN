a9342e6e5f2a8d429116692f295aa10ea76a439a
chrishor29 <chrishor29@gmail.com>
2016-12-27 23:41:36 +0100

    make some changes

--- update.bat
+++ update.bat
@@ -1,6 +1,11 @@
 copy F:\LEARN\javascript.js I:\Users\chrishor29\Desktop\learn\javascript.js
 copy F:\LEARN\style.css I:\Users\chrishor29\Desktop\learn\style.css
 
+robocopy F:\LEARN\done\KEMIA\images I:\Users\chrishor29\Desktop\learn\ok\images /mir
+copy F:\LEARN\done\KEMIA\kemia.html I:\Users\chrishor29\Desktop\learn\ok\kemia.html
+
+
+
 robocopy F:\LEARN\OB\images I:\Users\chrishor29\Desktop\learn\OB\images /mir
 copy F:\LEARN\OB\ob.html I:\Users\chrishor29\Desktop\learn\OB\ob.html
 
