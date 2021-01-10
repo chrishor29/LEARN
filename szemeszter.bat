6351a3cdd3a349f759a82eec4d9d486bdfb9edb0
chrishor29 <chrishor29@gmail.com>
2021-01-10 17:48:30 +0100

    make some changes

--- szemeszter.bat
+++ szemeszter.bat
@@ -18,6 +18,7 @@ xcopy /y %source%\favicon.png %destination%\ /d
 REM 5.év
 xcopy /y %source%\Belgyogy\HIIRLm\hiirlm.html %destination%\Belgyogy\HIIRLm\ /d
 xcopy /y %source%\Neurologia\neurologia.html %destination%\Neurologia\ /d
+xcopy /y %source%\Szemeszet\szemeszet.html %destination%\Szemeszet\ /d
 xcopy /y %source%\IgOr\igor.html %destination%\IgOr\ /d
 xcopy /y %source%\done\Urologia\urologia.html %destination%\done\Urologia\ /d
 
